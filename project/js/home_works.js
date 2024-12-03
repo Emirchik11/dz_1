8a2c5ef5319ff403d1398c3cd1e0364fc0235674
Emirchik11 <shakemidinovemir@gmail.com>
2024-12-03 16:15:48 +0600

    Add files via upload

---
+++ project/js/home_works.js
@@ -0,0 +1,46 @@
+// Homework 1 part 1
+
+
+    const gmailInput = document.getElementById('gmail_input');
+    const gmailButton = document.getElementById('gmail_button');
+    const gmailResult = document.getElementById('gmail_result');
+
+
+    const gmailRegExp = /^[a-zA-Z0-9._%]+@gmail\.com$/;
+
+
+
+    gmailButton.onclick = () => {
+    if (gmailRegExp.test(gmailInput.value)) {
+        gmailResult.textContent = 'OK';
+        gmailResult.style.color = 'green';
+    } else {
+
+        gmailResult.textContent = 'Invalid gmail';
+        gmailResult.style.color = 'red';
+    }
+};
+
+
+
+
+    const parentBlock = document.querySelector('.parent_block');
+    const childBlock = document.querySelector('.child_block');
+
+    const moveBlock = (position) => {
+        const parentWidth = parentBlock.clientWidth;
+        const childWidth = childBlock.clientWidth;
+
+        if (position < parentWidth - childWidth) {
+            childBlock.style.left = `${position}px`;
+
+
+            requestAnimationFrame(() => moveBlock(position + 1));
+        }
+    };
+
+
+    moveBlock(1);
+
+
+
